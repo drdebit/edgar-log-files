6c156152f8add0ccd14da39a7dc3efdadba8c3af
drdebit <mdeangelis@gsu.edu>
2020-12-02 15:32:44 -0500

    First commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to edgar-log-files
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/edgar_log_files/core_test.clj
@@ -0,0 +1,7 @@
+(ns edgar-log-files.core-test
+  (:require [clojure.test :refer :all]
+            [edgar-log-files.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
